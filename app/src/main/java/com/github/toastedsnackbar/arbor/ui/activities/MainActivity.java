62b9497230d3c4cd5662ab4a32f7845c984bd243
Odhita Kamayana <odhitakamayana@gmail.com>
2015-10-08 11:57:49 -0400

    Setup Robolectric and test environment

--- app/src/main/java/com/github/toastedsnackbar/arbor/ui/activities/MainActivity.java
+++ app/src/main/java/com/github/toastedsnackbar/arbor/ui/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.github.toastedsnackbar.arbor;
+package com.github.toastedsnackbar.arbor.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.github.toastedsnackbar.arbor.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/test/java/com/github/toastedsnackbar/arbor/ArborTestRunner.java
@@ -0,0 +1,54 @@
+package com.github.toastedsnackbar.arbor;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.manifest.AndroidManifest;
+import org.robolectric.res.Fs;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ArborTestRunner extends RobolectricGradleTestRunner {
+
+    private static final int MAX_SDK_LEVEL = 21;
+
+    private static final String PROJECT_DIR = getProjectDirectory();
+    private static final String MANIFEST_DIR = PROJECT_DIR + "src/main/AndroidManifest.xml";
+    private static final String RES_DIR = PROJECT_DIR + "build/intermediates/res/merged/debug";
+    private static final String ASSETS_DIR = PROJECT_DIR + "build/intermediates/assets/debug";
+
+    public ArborTestRunner(Class<?> c) throws InitializationError {
+        super(c);
+    }
+
+    @Override
+    protected AndroidManifest getAppManifest(Config config) {
+        return getAppManifest();
+    }
+
+    private static String getProjectDirectory() {
+        String path = "";
+
+        try {
+            File file = new File("..");
+            path = file.getCanonicalPath();
+            path += "/app/";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return path;
+    }
+
+    private static AndroidManifest getAppManifest() {
+        return new AndroidManifest(Fs.fileFromPath(MANIFEST_DIR), Fs.fileFromPath(RES_DIR),
+                Fs.fileFromPath(ASSETS_DIR)) {
+
+            @Override
+            public int getTargetSdkVersion() {
+                return MAX_SDK_LEVEL;
+            }
+        };
+    }
+}
