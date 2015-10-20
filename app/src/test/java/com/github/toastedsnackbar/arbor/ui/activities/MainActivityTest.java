0a903907ee66585bd6658b401b8ad2aa3d31db58
Odhita Kamayana <odhitakamayana@gmail.com>
2015-10-20 02:24:27 -0400

    OK [#105409798] - Updated MainActivityTest

--- app/src/test/java/com/github/toastedsnackbar/arbor/ui/activities/MainActivityTest.java
+++ app/src/test/java/com/github/toastedsnackbar/arbor/ui/activities/MainActivityTest.java
@@ -1,11 +1,17 @@
 package com.github.toastedsnackbar.arbor.ui.activities;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import com.github.toastedsnackbar.arbor.ArborTestRunner;
 import com.github.toastedsnackbar.arbor.R;
 import com.github.toastedsnackbar.arbor.net.ApiEndpoints;
+import com.github.toastedsnackbar.arbor.net.ApiReceiver;
+import com.github.toastedsnackbar.arbor.net.ApiService;
+import com.github.toastedsnackbar.arbor.net.requests.AccessTokenRequest;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,6 +19,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.internal.ShadowExtractor;
+import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowWebView;
 import org.robolectric.util.ActivityController;
 
@@ -21,6 +28,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(ArborTestRunner.class)
 public class MainActivityTest {
+
+    private static final String TEST_REDIRECT_URL = ApiEndpoints.getRedirectUrl()
+            + "?code=code&state=state";
 
     private MainActivity mActivity;
     private ActivityController<MainActivity> mActivityController;
@@ -70,7 +80,6 @@ public class MainActivityTest {
         loginBtn.performClick();
 
         WebView loginWebView = (WebView) mActivity.findViewById(R.id.web_view_login);
-        assertThat(loginWebView).isNotNull();
         assertThat(loginWebView).isVisible();
     }
 
@@ -80,12 +89,61 @@ public class MainActivityTest {
         loginBtn.performClick();
 
         WebView loginWebView = (WebView) mActivity.findViewById(R.id.web_view_login);
-        assertThat(loginWebView).isNotNull();
-
         ShadowWebView shadowLoginWebView = (ShadowWebView) ShadowExtractor.extract(loginWebView);
+
         String correctUrl = ApiEndpoints.getOAuthUrl();
         String actualUrl = shadowLoginWebView.getLastLoadedUrl();
         assertThat(actualUrl).isEqualTo(correctUrl);
+    }
+
+    @Test
+    public void oauth_onRedirect_loginWebViewShouldBeGone() {
+        Button loginBtn = (Button) mActivity.findViewById(R.id.btn_login);
+        loginBtn.performClick();
+
+        final WebView loginWebView = (WebView) mActivity.findViewById(R.id.web_view_login);
+        ShadowWebView shadowLoginWebView = (ShadowWebView) ShadowExtractor.extract(loginWebView);
+
+        WebViewClient webViewClient = shadowLoginWebView.getWebViewClient();
+        webViewClient.shouldOverrideUrlLoading(loginWebView, TEST_REDIRECT_URL);
+
+        assertThat(loginWebView).isGone();
+    }
+
+    @Test
+    public void oauth_onRedirect_loginButtonShouldBeGone() {
+        Button loginBtn = (Button) mActivity.findViewById(R.id.btn_login);
+        loginBtn.performClick();
+
+        WebView loginWebView = (WebView) mActivity.findViewById(R.id.web_view_login);
+        loginWebView.loadUrl(TEST_REDIRECT_URL);
+
+        assertThat(loginBtn).isGone();
+    }
+
+    @Test
+    public void oauth_onRedirect_shouldStartApiServiceWithCorrectExtras() {
+        Button loginBtn = (Button) mActivity.findViewById(R.id.btn_login);
+        loginBtn.performClick();
+
+        WebView loginWebView = (WebView) mActivity.findViewById(R.id.web_view_login);
+        ShadowWebView shadowLoginWebView = (ShadowWebView) ShadowExtractor.extract(loginWebView);
+
+        WebViewClient webViewClient = shadowLoginWebView.getWebViewClient();
+        webViewClient.shouldOverrideUrlLoading(loginWebView, TEST_REDIRECT_URL);
+
+        ShadowActivity shadowMainActivity = (ShadowActivity) ShadowExtractor.extract(mActivity);
+        Intent startedIntent = shadowMainActivity.getNextStartedService();
+        assertThat(startedIntent).isNotNull();
+        assertThat(startedIntent).hasComponent(new ComponentName(mActivity, ApiService.class));
+
+        assertThat(startedIntent).hasExtra(ApiService.EXTRA_REQUEST);
+        assertThat(startedIntent.getParcelableExtra(ApiService.EXTRA_REQUEST)).isInstanceOf(
+                AccessTokenRequest.class);
+
+        assertThat(startedIntent).hasExtra(ApiService.EXTRA_RESULT_RECEIVER);
+        assertThat(startedIntent.getParcelableExtra(ApiService.EXTRA_RESULT_RECEIVER)).isInstanceOf(
+                ApiReceiver.class);
     }
 
     @Test
